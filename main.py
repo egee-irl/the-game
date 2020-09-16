a83c948e987eea25769bebfc5fafd037d8c6946f
egee-irl <brian@egee.io>
2020-09-15 17:49:26 -0700

    bears

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from math import pi, sin, cos
+
 from direct.showbase.ShowBase import ShowBase
+from direct.actor.Actor import Actor
+from direct.task import Task
 
 
 class MyApp(ShowBase):
@@ -8,11 +12,28 @@ class MyApp(ShowBase):
 
         # Load the environment model.
         self.scene = self.loader.loadModel("models/environment")
+
         # Reparent the model to render.
         self.scene.reparentTo(self.render)
+
         # Apply scale and position transforms on the model.
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, -2)
+
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
+        self.pandaActor = Actor("models/panda-model",
+                                {"walk": "models/panda-walk4"})
+        self.pandaActor.setScale(0.005, 0.005, 0.005)
+        self.pandaActor.reparentTo(self.render)
+        self.pandaActor.loop("walk")
+
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 10
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
 
 
 app = MyApp()
