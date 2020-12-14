c1c716e03210303897e41a090f2d749001ec6c37
egee-irl <brian@egee.io>
2020-12-13 17:55:06 -0800

    changed stuff

--- EgeeGame/Duck.gd
+++ EgeeGame/Duck.gd
@@ -1,10 +1,9 @@
 extends KinematicBody2D
 
 
-export (int) var speed = 200
+export (int) var speed = 100
 export (int) var jump_speed = -800
-export (int) var gravity = 4000
-
+export (int) var gravity = 3000
 var velocity = Vector2.ZERO
 
 func get_input():
@@ -21,6 +20,16 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_up"):
 		if is_on_floor():
 			velocity.y = jump_speed
+	elif Input.is_action_pressed("ui_right"):
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("walking")
+	elif Input.is_action_pressed("ui_left"):
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("walking")
+	else:
+		$AnimatedSprite.play("standing")
+	if !is_on_floor():
+		$AnimatedSprite.play("jumping")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -30,4 +39,3 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
